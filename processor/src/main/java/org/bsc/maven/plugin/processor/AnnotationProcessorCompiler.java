e0adf9a03b6134758289b8d757100b08647efe7a
bartolomeo sorrentino <bartolomeo.sorrentino@gmail.com>
2020-07-28 15:09:24 +0200

    issue #82 - fix 'UnsupportedOperationException' on isSupportedOption invocation

--- processor/src/main/java/org/bsc/maven/plugin/processor/AnnotationProcessorCompiler.java
+++ processor/src/main/java/org/bsc/maven/plugin/processor/AnnotationProcessorCompiler.java
@@ -233,7 +233,7 @@ class PlexusJavaCompilerWithOutput {
 }
 /**
  *
- * @author softphone
+ * @author bsorrentino
  */
 public class AnnotationProcessorCompiler implements JavaCompiler {
     private static final String COMPILER_TARGET = "maven.compiler.target";
@@ -480,12 +480,12 @@ public class AnnotationProcessorCompiler implements JavaCompiler {
 
     @Override
     public Set<SourceVersion> getSourceVersions() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return systemJavaCompiler.getSourceVersions();
     }
 
     @Override
     public int isSupportedOption(String option) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        return systemJavaCompiler.isSupportedOption(option);
     }
 
 }
