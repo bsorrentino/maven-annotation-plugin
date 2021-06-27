ce0b27953216d3775b6f3c8e364752c826e0a685
Build Pipeline <build.pipeline@softphone.it>
2021-06-27 22:11:55 +0200

    PR #45 - add unit test

--- processor/src/test/java/org/bsc/maven/plugin/processor/AnnotationProcessorMojoTest.java
+++ processor/src/test/java/org/bsc/maven/plugin/processor/AnnotationProcessorMojoTest.java
@@ -1,23 +1,50 @@
 package org.bsc.maven.plugin.processor;
 
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.Mojo;
 import org.apache.maven.plugin.testing.MojoRule;
 import org.apache.maven.plugin.testing.resources.TestResources;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
-public class MojoTest   {
+import static org.junit.Assert.*;
+
+public class AnnotationProcessorMojoTest {
 
   @Rule
   public MojoRule mojoRule = new MojoRule();
 
-  @Rule public TestResources resources = new TestResources();
+  @Rule
+  public TestResources resources = new TestResources();
+
+  final Path outputPath = Paths.get("target", "classes");
 
   @Test
-  public void execute() throws Exception {
-    File pom = new File(resources.getBasedir("test-project-1"), "pom.xml");
-//    MainMojo mojo = (MainMojo) mojoRule.lookupMojo("translate", pom);
-//    mojo.execute();
+  public void testPR45() throws Exception {
+    final File pom = Paths.get(outputPath.toString(), "pr45", "pom.xml").toFile();
+
+    assertNotNull(pom);
+
+    final File baseDir = Paths.get(outputPath.toString(), "pr45").toFile();
+    assertNotNull(baseDir);
+    assertTrue( baseDir.exists() );
+    assertTrue( baseDir.isDirectory() );
+
+    final Mojo mojo = mojoRule.lookupConfiguredMojo(baseDir, "process");
+
+    assertTrue(mojo instanceof MainAnnotationProcessorMojo);
+
+    final MainAnnotationProcessorMojo myMojo = (MainAnnotationProcessorMojo) mojo;
+
+    myMojo.execute();
+
+    assertNotNull( myMojo.annotationProcessorPaths );
+    assertEquals( 1, myMojo.annotationProcessorPaths.size() );
+
   }
 }
