72dcb6c47460ecbeb44121eb49d4d7e6f565edf4
bsorrentino <bartolomeo.sorrentino@gmail.com>
2021-10-18 20:02:52 +0200

    feat(graaljs): add ad-hoc test module

---
+++ test/graaljs/README.md
@@ -0,0 +1,3 @@
+# Maven Annotation plugin - Test using Graaljs
+
+_TO DO_
---
+++ test/graaljs/src/main/java/org/bsc/processor/test/SampleClass.java
@@ -0,0 +1,12 @@
+package org.bsc.maven.plugin.processor.test;
+
+public class SampleClass {
+
+    final public String id;
+    final public String name;
+
+    public SampleClass(String id, String name ) {
+        this.name = name;
+        this.id = id;
+    }
+}
