cb202d89c864406157001d0a51bce25833469295
Henri Yandell <hyandell@amazon.com>
2024-10-17 17:00:22 -0700

    Adjusting licensing to MIT per #105

--- processor/src/main/java/org/bsc/maven/plugin/processor/AbstractAnnotationProcessorMojo.java
+++ processor/src/main/java/org/bsc/maven/plugin/processor/AbstractAnnotationProcessorMojo.java
@@ -1,20 +1,6 @@
 /*
  *   Copyright (C) 2009 2010 2011 Bartolomeo Sorrentino <bartolomeo.sorrentino@gmail.com>
- *
- *   This file is part of maven-annotation-plugin.
- *
- *    maven-annotation-plugin is free software: you can redistribute it and/or modify
- *    it under the terms of the GNU Lesser General Public License as published by
- *    the Free Software Foundation, either version 3 of the License, or
- *    (at your option) any later version.
- *
- *    maven-annotation-plugin is distributed in the hope that it will be useful,
- *    but WITHOUT ANY WARRANTY; without even the implied warranty of
- *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *    GNU General Public License for more details.
- *
- *    You should have received a copy of the GNU Lesser General Public License
- *    along with maven-annotation-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ *   SPDX-License-Identifier: MIT
  */
 
 package org.bsc.maven.plugin.processor;
